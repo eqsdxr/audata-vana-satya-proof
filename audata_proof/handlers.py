7be2b9ebffc0f3219d46868a4fc7eff67558a559
eqsdxr <rxdsqe@gmail.com>
2025-04-05 20:21:01 +0500

    feat: handle uniquness field
    - add logic to handle uniqueness, it's still not implemented fully
    yet due to technical reasons
    - turn uniqueness handler into a regular function for simplicity
    - replace logging from standard library with logger from loguru
    - some minor changes

--- audata_proof/handlers.py
+++ audata_proof/handlers.py
@@ -1,3 +1,5 @@
+from hashlib import md5
+
 from acoustid import fingerprint_file, compare_fingerprints
 
 from audata_proof.db import db
@@ -9,35 +11,32 @@ from audata_proof.models.db import Contributions
 from audata_proof.config import logger
 
 
-class Uniqueness:
-    def __call__(self, file_path: str, similarity_threshold: float = 0.8):
-        # Get the fingerprint of the input file
-        current_fprint, _ = fingerprint_file(file_path)
-
-        with db.session() as session:
-            # Check for exactly the same one
-            duplicate = (
-                session.query(Contributions)
-                .filter_by(fingerprint=current_fprint)
-                .first()
+def check_uniqueness(file_path: str, similarity_threshold: float = 0.8):
+    current_duration, current_fprint = fingerprint_file(file_path) # getting fingerprint
+    db.init()
+    with db.session() as session:
+        # Check for exactly the same one
+        duplicate = (
+            session.query(Contributions)
+            .filter_by(fingerprint=str(current_fprint))
+            .one_or_none()
+        )
+        if duplicate:
+            logger.info(
+                "Exact fingerprint match found: {} {}".format(
+                    current_fprint, duplicate.fingerprint
+                )
             )
-            if duplicate:
+            raise FingerprintAlreadyExists()
+
+        # Loop through db fingerprints and compare
+        for contribution in session.query(Contributions).all():
+            similarity_score = 0.4 #compare_fingerprints(
+            #    (current_duration, current_fprint), (contribution.duration, contribution.fingerprint))
+            #)
+
+            if similarity_score >= similarity_threshold:
                 logger.info(
-                    "Exact fingerprint match found: {} {}".format(
-                        current_fprint, duplicate
-                    )
+                    f"Similar fingerprint found (Score: {similarity_score}): {current_fprint} and {contribution.fingerprint}"
                 )
-                raise FingerprintAlreadyExists()
-
-            # Loop through db fingerprints and compare
-            for db_fprint in session.query(Contributions).all():
-                fprint2 = db_fprint.fingerprint
-                similarity_score = compare_fingerprints(
-                    current_fprint, fprint2
-                )
-
-                if similarity_score >= similarity_threshold:
-                    logger.info(
-                        f"Similar fingerprint found (Score: {similarity_score}): {current_fprint} and {fprint2}"
-                    )
-                    raise TooSimilarFingerprintAlreadyExists()
+                raise TooSimilarFingerprintAlreadyExists()
