9cb2ab83d5a89cd3eaf37f05b877ed20b54e7a85
eqsdxr <rxdsqe@gmail.com>
2025-04-05 17:23:21 +0500

    feat: add uniqueness handler

--- audata_proof/handlers.py
+++ audata_proof/handlers.py
@@ -1,15 +1,43 @@
-import fingerprint
+from acoustid import fingerprint_file, compare_fingerprints
 
-from audata_proof import db
+from audata_proof.db import db
+from audata_proof.exc import (
+    FingerprintAlreadyExists,
+    TooSimilarFingerprintAlreadyExists,
+)
+from audata_proof.models.db import Contributions
+from audata_proof.config import logger
+
 
 class Uniqueness:
-    def get_file_fingerprint(self, file_path: str) -> list[tuple]:
-        fprint = fingerprint.Fingerprint(
-            kgram_len=4, window_len=5, base=10, modulo=1000
-        )
-        fprint_hash: list[tuple] = fprint.generate(fpath=file_path)
-        return fprint_hash
+    def __call__(self, file_path: str, similarity_threshold: float = 0.8):
+        # Get the fingerprint of the input file
+        current_fprint, _ = fingerprint_file(file_path)
 
-    def is_unique(self, file_path: str):
-        fprint: list[tuple] = self.get_file_fingerprint(file_path)
+        with db.session() as session:
+            # Check for exactly the same one
+            duplicate = (
+                session.query(Contributions)
+                .filter_by(fingerprint=current_fprint)
+                .first()
+            )
+            if duplicate:
+                logger.info(
+                    "Exact fingerprint match found: {} {}".format(
+                        current_fprint, duplicate
+                    )
+                )
+                raise FingerprintAlreadyExists()
 
+            # Loop through db fingerprints and compare
+            for db_fprint in session.query(Contributions).all():
+                fprint2 = db_fprint.fingerprint
+                similarity_score = compare_fingerprints(
+                    current_fprint, fprint2
+                )
+
+                if similarity_score >= similarity_threshold:
+                    logger.info(
+                        f"Similar fingerprint found (Score: {similarity_score}): {current_fprint} and {fprint2}"
+                    )
+                    raise TooSimilarFingerprintAlreadyExists()
