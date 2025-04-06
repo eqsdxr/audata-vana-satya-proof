2b74b430e82a44ab6b37e781ccc9abe2b2064b03
eqsdxr <rxdsqe@gmail.com>
2025-04-06 14:49:47 +0500

    refactor: rearrange columns for better readability and add a comment

--- audata_proof/models/db.py
+++ audata_proof/models/db.py
@@ -31,21 +31,23 @@ class Contributions(Base):
     __tablename__ = 'contributions'
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-    # Technically, the same fingerprint can be written
-    # in database because if we add parameter "unique=True"
-    # here, the value will be too long to be handled as unique
-    # by PostgreSQL
-    fingerprint = Column(Text, nullable=False)
-    # Store hash for fast uniquness lookups.
-    fingerprint_hash = Column(
-        String(32), unique=True, nullable=False
-    )  # 32 chars for md5
+    # Store duration for accurate fingerprint comparisons
+    duration = Column(Float, nullable=False)
+    uploaded_at = Column(DateTime(timezone=True), server_default=func.now())
     # Again, "unique=True" is not used here because if links
     # are too long so it's better to use hash instead
     file_link = Column(Text, nullable=False)
     file_link_hash = Column(
         String(32), unique=True, nullable=False
     )  # 32 chars for md5
-    uploaded_at = Column(DateTime(timezone=True), server_default=func.now())
-    # Store duration for accurate fingerprint comparisons
-    duration = Column(Float, nullable=False, default=30.0)
+    # Technically, the same fingerprint can be written
+    # in database because if we add parameter "unique=True"
+    # here, the value will be too long to be handled as unique
+    # by PostgreSQL.
+    # Also, when a fingerprint is stored in PostgreSQL it's converted
+    # into raw bytes, so it's required to decode them before using
+    fingerprint = Column(Text, nullable=False)
+    # Store hash for fast uniquness lookups.
+    fingerprint_hash = Column(
+        String(32), unique=True, nullable=False
+    )  # 32 chars for md5
