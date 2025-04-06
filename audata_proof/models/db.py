c1b76da6abda8b788d012809743d9a46ad5f9f39
eqsdxr <rxdsqe@gmail.com>
2025-04-06 10:15:51 +0500

    feat(db_models): add file_link_hash and useful comments

--- audata_proof/models/db.py
+++ audata_proof/models/db.py
@@ -31,10 +31,17 @@ class Contributions(Base):
     __tablename__ = "contributions"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    # Technically, the same fingerprint can be written
+    # in database because if we add parameter "unique=True"
+    # here, the value will be too long to be handled as unique
+    # by PostgreSQL
     fingerprint = Column(Text, nullable=False)
-    # Store hash too for fast uniquness lookups
+    # Store hash for fast uniquness lookups.
     fingerprint_hash = Column(String(32), unique=True, nullable=False) # 32 chars for md5
-    file_link = Column(Text, unique=True, nullable=False)
+    # Again, we don't use "unique=True" here because if the link
+    # is too long it's better to use hash instead
+    file_link = Column(Text, nullable=False)
+    file_link_hash = Column(String(32), unique=True, nullable=False) # 32 chars for md5
     uploaded_at = Column(DateTime(timezone=True), server_default=func.now())
     # Store duration for accurate fingerprint comparisons
     duration = Column(Float, nullable=False, default=30.0)
